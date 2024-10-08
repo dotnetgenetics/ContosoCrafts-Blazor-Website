29fdeba9270e9b615e6790391a91baa488db624f
Gregory Nelson Esguerra <gregorye@ggsinc.com>
2024-10-08 18:41:18 +0800

    Add project files.

---
+++ ContosoCrafts.BlazorWebsite/Infrastructure/Contracts/IJsonFileProductService.cs
@@ -0,0 +1,10 @@
+﻿using ContosoCrafts.BlazorWebsite.Model;
+
+namespace ContosoCrafts.BlazorWebsite.Infrastructure.Contracts
+{
+    public interface IJsonFileProductService
+    {
+        public IEnumerable<Product> GetProducts();
+        public void AddRating(string productId, int rating);
+    }
+}
---
+++ ContosoCrafts.BlazorWebsite/Infrastructure/Services/JsonFileProductService.cs
@@ -0,0 +1,64 @@
+﻿using ContosoCrafts.BlazorWebsite.Infrastructure.Contracts;
+using ContosoCrafts.BlazorWebsite.Model;
+using System.Text.Json;
+
+namespace ContosoCrafts.BlazorWebsite.Infrastructure.Services
+{
+    public class JsonFileProductService : IJsonFileProductService
+    {
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "Data", "products.json"); }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            var query = products.First(x => x.Id == productId);
+
+            if (query.Ratings == null)
+            {
+                query.Ratings = new[] { rating };
+            }
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            //serialize
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                );
+            }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+}
